a3c644549e7b2c5b7f2026ace3f585c20de6881a
Tawhid-01 <mtworld008@gmail.com>
2025-08-20 16:31:30 +0600

    new section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import LogoSection from './sections/LogoSection'
 import FeatureCards from './sections/FeatureCards'
 import ExperienceSection from './sections/ExperienceSection'
 import TechStack from './sections/TechStack'
+import { Testimonials } from './sections/Testimonials'
+
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
    < FeatureCards/>
    <ExperienceSection/>
    <TechStack />
+   <Testimonials />
    </>
     
   )
