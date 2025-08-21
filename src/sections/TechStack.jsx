0fc9b64c13055c9f6a8f349177b108bc448b6a7f
Tawhid-01 <mtworld008@gmail.com>
2025-08-21 19:16:27 +0600

    change

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -22,7 +22,7 @@ const TechStack = () => {
   )
   })
   return (
-    <div id='skills' className='flex-center section-padding'>
+    <div id='skills' className='flex-center section-padding mb-20'>
         <div className="w-full h-full md:px-10 px-5">
             <TitleHeader
 
